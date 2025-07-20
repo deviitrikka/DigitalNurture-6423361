0c80d6a662e2e9dd71ad1c7481099bfcc85a4d2c
deviitrikka <aitrika09@gmail.com>
2025-07-20 16:17:21 +0530

    adding week6 ex 3 solutions

---
+++ week_6/exercise3/scorecalculatorapp/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import { CalculateScore } from './components/CalculateScore';
+
+function App() {
+  return (
+    <>
+    <CalculateScore Name={"Steeve"}
+    School={"DNV Public School"}
+    total={284}
+    goal={3}
+    />
+    </>
+    
+  );
+}
+
+export default App;
